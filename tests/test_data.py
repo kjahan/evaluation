c4444ad0e13fb2cee638f284b4c296486ecb8a65
Kazem Jahanbakhsh <k.jahanbakhsh@gmail.com>
2021-01-17 18:46:27 -0800

    refactor tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,12 +4,17 @@ import pandas as pd
 from src.dataset import split
 
 
-def test_split():
+def mock_data():
     f = lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f')
     dates = ['2020-08-01 10:24:01.621881', '2020-08-02 11:24:01.621881', '2020-08-03 17:24:01.621881',\
              '2020-08-04 15:24:01.621881', '2020-08-05 02:24:01.621881']
     df = pd.DataFrame.from_dict({'user_id': [1, 2, 3, 4, 5], 'item_id': [10, 20, 30, 40, 50], \
             'datetime': list(map(f, dates))})
+    return df
+
+
+def test_split():
+    df = mock_data()
     split_time = datetime.strptime('2020-08-04', '%Y-%m-%d')
     train, test = split(df, split_time)
     assert train.shape[0] == 3
@@ -17,11 +22,7 @@ def test_split():
 
 
 def test_split_empty_train():
-    f = lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f')
-    dates = ['2020-08-01 10:24:01.621881', '2020-08-02 11:24:01.621881', '2020-08-03 17:24:01.621881',\
-             '2020-08-04 15:24:01.621881', '2020-08-05 02:24:01.621881']
-    df = pd.DataFrame.from_dict({'user_id': [1, 2, 3, 4, 5], 'item_id': [10, 20, 30, 40, 50], \
-            'datetime': list(map(f, dates))})
+    df = mock_data()
     split_time = datetime.strptime('2020-07-29', '%Y-%m-%d')
     train, test = split(df, split_time)
     assert train.empty
@@ -29,11 +30,7 @@ def test_split_empty_train():
 
 
 def test_split_empty_test():
-    f = lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f')
-    dates = ['2020-08-01 10:24:01.621881', '2020-08-02 11:24:01.621881', '2020-08-03 17:24:01.621881',\
-             '2020-08-04 15:24:01.621881', '2020-08-05 02:24:01.621881']
-    df = pd.DataFrame.from_dict({'user_id': [1, 2, 3, 4, 5], 'item_id': [10, 20, 30, 40, 50], \
-            'datetime': list(map(f, dates))})
+    df = mock_data()
     split_time = datetime.strptime('2020-08-06', '%Y-%m-%d')
     train, test = split(df, split_time)
     assert train.shape[0] == df.shape[0]
